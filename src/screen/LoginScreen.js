280de2bd4789c910df16b42e457b8996af2eb296
Firagos <jemalfiragos@gmail.com>
2025-07-28 11:38:12 +0300

    finally making the password field to hide the password

--- src/screen/LoginScreen.js
+++ src/screen/LoginScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, KeyboardAvoidingView, Platform } from "react-native";
 import { TextInput, Button, Text, Title } from "react-native-paper";
 import { Formik } from "formik";
@@ -10,6 +10,7 @@ const LoginSchema = Yup.object().shape({
 });
 
 export default function LoginScreen({ navigation }) {
+  const [passwordVisible, setPasswordVisible] = useState(false);
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -48,12 +49,18 @@ export default function LoginScreen({ navigation }) {
             <TextInput
               label="Password"
               mode="outlined"
-              secureTextEntry
+              secureTextEntry={!passwordVisible}
               value={values.password}
               onChangeText={handleChange("password")}
               onBlur={handleBlur("password")}
               style={styles.input}
               error={touched.password && errors.password}
+              right={
+                <TextInput.Icon
+                  icon={passwordVisible ? "eye-off" : "eye"}
+                  onPress={() => setPasswordVisible(!passwordVisible)}
+                />
+              }
             />
             {touched.password && errors.password && (
               <Text style={styles.error}>{errors.password}</Text>
