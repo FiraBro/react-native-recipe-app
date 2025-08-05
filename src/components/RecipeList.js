99f964ec191b748d6a1a19f6d3f637e07bdcda6c
Firagos <jemalfiragos@gmail.com>
2025-08-05 11:44:29 +0300

    adding moer future to it

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,10 +4,12 @@ import { FlatList, Text, View, ActivityIndicator, Image } from "react-native";
 export default function RecipeList({ categoryId }) {
   const [recipes, setRecipes] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchRecipes = async () => {
     try {
       setLoading(true);
+      setError(null);
       const res = await fetch(
         `https://ecomerceapi-3.onrender.com/api/v1/category/${categoryId}/products`
       );
@@ -16,6 +18,7 @@ export default function RecipeList({ categoryId }) {
       console.log("Fetched recipes:", data.products);
     } catch (err) {
       console.error("Failed to fetch recipes", err);
+      setError("Failed to load recipes");
       setRecipes([]);
     } finally {
       setLoading(false);
@@ -28,6 +31,10 @@ export default function RecipeList({ categoryId }) {
 
   if (loading) {
     return <ActivityIndicator style={{ marginTop: 20 }} size="large" />;
+  }
+
+  if (error) {
+    return <Text style={{ textAlign: "center", marginTop: 20 }}>{error}</Text>;
   }
 
   if (!recipes.length) {
@@ -43,9 +50,7 @@ export default function RecipeList({ categoryId }) {
       data={recipes}
       keyExtractor={(item) => item._id}
       renderItem={({ item }) => (
-        <View
-          style={{ padding: 16, borderBottomWidth: 1, borderColor: "#eee" }}
-        >
+        <View style={{ borderBottomWidth: 1, borderColor: "#eee" }}>
           <Image
             source={{ uri: item.image }}
             style={{ width: "100%", height: 200, borderRadius: 10 }}
@@ -53,7 +58,9 @@ export default function RecipeList({ categoryId }) {
           />
           <Text style={{ fontSize: 18, fontWeight: "bold" }}>{item.title}</Text>
           <Text>{item.description}</Text>
-          <Text style={{color:'#000',fontSize:22,fontFamily:"bold"}}>${item.price}</Text>
+          <Text style={{ color: "#000", fontSize: 22, fontFamily: "bold" }}>
+            ${item.price}
+          </Text>
         </View>
       )}
     />
